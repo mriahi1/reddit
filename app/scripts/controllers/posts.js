6d25975192e97c0cf95c7fcf06e6606079c8fbd7
Maxime Riahi <maxriahi@gmail.com>
2015-02-13 17:34:44 -0500

    add style

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -5,14 +5,18 @@ app.controller('PostsCtrl', function ($scope, $location, Post) {
 
   $scope.post = {url: 'http://', 'title': ''};
 
+  $scope.deletePost = function (post) {
+    Post.delete(post);
+  };
+
+});
+
+app.controller('NavCtrl', function ($scope, $location, Post) {
+  $scope.post = {url: 'http://', 'title': ''};
+
   $scope.submitPost = function () {
     Post.create($scope.post).then(function(ref) {
       $location.path('/posts/' + ref.name());
     });
   };
-
-  $scope.deletePost = function (post) {
-    Post.delete(post);
-  };
-
 });
